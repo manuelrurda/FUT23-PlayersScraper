5380a1ae8f8c9d3969d8ed056b706b7000a67367
manuelrurda <manuelrodriguezurda@Manuels-MacBook-Air.local>
2022-10-13 10:32:37 -0500

    deleted accidental import

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from lib2to3.pgen2 import driver
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
